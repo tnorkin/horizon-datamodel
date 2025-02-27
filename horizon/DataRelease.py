50832f747f1bd81e040b99eef44c7f19a4566e27
Tamar Norkin <tnorkin@usgs.gov>
2025-02-27 14:59:19 -0700

    added "sensitive" data type to class UsgsReleaseTypeEnum, alphebetized classes StatusEnum and UsgsReleaseTypeEnum

--- horizon/DataRelease.py
+++ horizon/DataRelease.py
@@ -7,30 +7,30 @@ class StatusEnum(str, Enum):
 
     created = "Created"
         Default status when a record is first created in the repository.
-    submitted = "Submitted"
-        Data release has been submitted to the curation team
-    locked = "Locked"
-        Data release is not public but has been locked to edits to allow for an external review period.
-    provisional = "Provisional"
-        Data release is subject to revision and has been released prior to Bureau approval to meet an immediate need.
-    published = "Published"
-        Data release has received Bureau approval and is publicly available.
-    underRevision = "Under Revision"
-        Data release is being revised. The dataset metadata may be available but files are not accessible.
-    revised = "Revised"
-        Data release has been revised.
     deprecated = "Deprecated"
         Data release was previously published but access to the data files needed to be removed due to errors or sensitivity converns.
+    locked = "Locked"
+        Data release is not public but has been locked to edits to allow for an external review period.
+    published = "Published"
+        Data release has received Bureau approval and is publicly available.
+    revised = "Revised"
+        Data release has been revised.
+    provisional = "Provisional"
+        Data release is subject to revision and has been released prior to Bureau approval to meet an immediate need.
+    submitted = "Submitted"
+        Data release has been submitted to the curation team
+    underRevision = "Under Revision"
+        Data release is being revised. The dataset metadata may be available but files are not accessible.
     """
 
     created = "Created"
-    submitted = "Submitted"
+    deprecated = "Deprecated"
     locked = "Locked"
     provisional = "Provisional"
     published = "Published"
-    underRevision = "Under Revision"
     revised = "Revised"
-    deprecated = "Deprecated"
+    submitted = "Submitted"
+    underRevision = "Under Revision"
 
 
 class UsgsReleaseTypeEnum(str, Enum):
@@ -42,6 +42,8 @@ class UsgsReleaseTypeEnum(str, Enum):
         Data that are continually collected, processed, quality-controlled, and released in real-time or near-real-time.
     legacy = "Legacy"
         Data that were released prior to the implementation of USGS Survey Manual Chapter 502.8.
+    sensitive = "Sensitive"
+        Data that, if made public, would result in an adverse effect on a taxon or a living individual or data that may have significant economic implications if disclosed prior to public release.
     softwareRelease = "Software Release"
         Software that were released prior to the requirement to publish software at code.usgs.gov.
     """
